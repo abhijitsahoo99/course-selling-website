fe9ac9cd49e72842ca3bfb37cc3a120df5b8c48a
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2023-10-16 00:20:13 +0530

    checkpoint-3 use of recoil for optimisation from lifting the state up #checkpoint-2

--- admin-client/src/components/Landing.jsx
+++ admin-client/src/components/Landing.jsx
@@ -1,22 +1,26 @@
 import {Grid, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import {useNavigate} from "react-router-dom";
+import {useRecoilValue} from "recoil";
+import { userEmailState } from "../store/selectors/userEmail"
+import {isUserLoading} from "../store/selectors/isUserLoading.js";
+import { IMAGE_URL } from "../config";
 
-const Landing = ({userEmail}) => {
+const Landing = () => {
     const navigate = useNavigate()
-
+    const userEmail = useRecoilValue(userEmailState);
+    const userLoading = useRecoilValue(isUserLoading);
     return <div>
         <Grid container style={{padding: "5vw"}}>
             <Grid item xs={12} md={6} lg={6}>
                 <div style={{marginTop: 100}}>
                     <Typography variant={"h2"}>
-                        Coursera Admin
+                        Coursera Admin Dashboard
                     </Typography>
                     <Typography variant={"h5"}>
                         A place to learn, earn and grow
                     </Typography>
-                    {!userEmail && 
-                        <div style={{display: "flex", marginTop: 20}}>
+                    {!userLoading && !userEmail && <div style={{display: "flex", marginTop: 20}}>
                         <div style={{marginRight: 10}}>
                             <Button
                                 size={"large"}
@@ -41,9 +45,9 @@ const Landing = ({userEmail}) => {
                 </div>
             </Grid>
             <Grid item xs={12} md={6} lg={6}  style={{marginTop: 20}}>
-                <img src={"/class.jpeg"} width={"100%"} />
+                <img src={IMAGE_URL} width={"100%"} />
             </Grid>
         </Grid>
     </div>
-} 
-export default Landing;
+}
+export default Landing ;
--- admin-client/src/components/Signin.jsx
+++ admin-client/src/components/Signin.jsx
@@ -4,11 +4,15 @@ import {Card, Typography} from "@mui/material";
 import {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {useSetRecoilState} from "recoil";
+import {userState} from "../store/atoms/user.js";
+import { BASE_URL } from "../config.js";
 
-function Signin({setUserEmail}) {
-    const navigate = useNavigate()
+function Signin() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+    const setUser = useSetRecoilState(userState);
 
     return <div>
             <div style={{
@@ -18,7 +22,7 @@ function Signin({setUserEmail}) {
                 justifyContent: "center"
             }}>
                 <Typography variant={"h6"}>
-                Welcome to Coursera. Sign up below
+                Welcome to Coursera. Sign in below
                 </Typography>
             </div>
         <div style={{display: "flex", justifyContent: "center"}}>
@@ -48,7 +52,7 @@ function Signin({setUserEmail}) {
                     size={"large"}
                     variant="contained"
                     onClick={async () => {
-                        const res = await axios.post("http://localhost:3000/admin/login", {
+                        const res = await axios.post(`${BASE_URL}/admin/login`, {
                             username: email,
                             password: password
                         }, {
@@ -57,9 +61,13 @@ function Signin({setUserEmail}) {
                             }
                         });
                         const data = res.data;
-                        
+
                         localStorage.setItem("token", data.token);
-                        setUserEmail(email);
+                        // window.location = "/"
+                        setUser({
+                            userEmail: email,
+                            isLoading: false
+                        })
                         navigate("/courses")
                     }}
 
